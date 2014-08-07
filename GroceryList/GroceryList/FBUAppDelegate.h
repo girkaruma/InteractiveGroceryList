0a193ec9b0b24459298c8d1ef2632f1239134f9e
Jennifer Zhang <jenniferjzhang@fb.com>
2014-08-06 18:31:28 -0700

    XCode project file uploaded

---
+++ GroceryList/GroceryList/FBUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FBUAppDelegate.h
+//  GroceryList
+//
+//  Created by Jennifer Zhang on 8/6/14.
+//  Copyright (c) 2014 FacebookU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FBUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GroceryList/GroceryList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GroceryList
+//
+//  Created by Jennifer Zhang on 8/6/14.
+//  Copyright (c) 2014 FacebookU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FBUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FBUAppDelegate class]));
+    }
+}
